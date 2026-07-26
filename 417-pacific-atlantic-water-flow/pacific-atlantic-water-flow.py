f27d3afe074cd6887d6e5107421cbfc9dd1d04be
sagarhipparagi <sagarhipparagi9@gmail.com>
2026-07-26 16:14:00 +0530

    Time: 39 ms (83.20%) | Memory: 13.8 MB (56.48%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def pacificAtlantic(self, heights):
+        """
+        :type heights: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        ROWS, COLS = len(heights), len(heights[0])
+
+        pacific = set()
+        atlantic = set()
+
+        def dfs(r, c, visited, prevHeight):
+            if (
+                r < 0 or c < 0 or
+                r >= ROWS or c >= COLS or
+                (r, c) in visited or
+                heights[r][c] < prevHeight
+            ):
+                return
+
+            visited.add((r, c))
+
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
+
+        # Pacific Ocean (top row and left column)
+        for c in range(COLS):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(ROWS - 1, c, atlantic, heights[ROWS - 1][c])
+
+        # Atlantic Ocean (bottom row and right column)
+        for r in range(ROWS):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, COLS - 1, atlantic, heights[r][COLS - 1])
+
+        result = []
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pacific and (r, c) in atlantic:
+                    result.append([r, c])
+
+        return result

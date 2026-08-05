9b249e298634bf2ff658cdde7b6450ba20e29132
sagarhipparagi <sagarhipparagi9@gmail.com>
2026-08-05 09:18:09 +0530

    Time: 11 ms (58.39%) | Memory: 13.2 MB (55.46%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_prod = nums[0]
+        cur_max = nums[0]
+        cur_min = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                cur_max, cur_min = cur_min, cur_max
+
+            cur_max = max(nums[i], cur_max * nums[i])
+            cur_min = min(nums[i], cur_min * nums[i])
+
+            max_prod = max(max_prod, cur_max)
+
+        return max_prod

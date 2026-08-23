791aa44aea04a099821be83fd8a43468b09f66a3
sagarhipparagi <sagarhipparagi9@gmail.com>
2026-08-23 12:21:12 +0530

    Time: 7 ms (71.67%) | Memory: 17.8 MB (54.83%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        bits = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            bits[i] = bits[i >> 1] + (i & 1)
+
+        return bits
